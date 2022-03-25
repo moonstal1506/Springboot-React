0f32b412b3ac1e889c1b8e84015d2b319ed58e19
moonstal1506 <moonstal1506@gmail.com>
2022-03-25 23:03:37 +0900

    화면구성

--- README.md
+++ README.md
@@ -13,3 +13,19 @@
 - yarn add react-router-dom@5.2.0
 - yarn add redux react-redux
 - yarn add react-bootstrap bootstrap
+
+```txt
+import 'bootstrap/dist/css/bootstrap.min.css';
+```
+
+### 프로젝트 세팅
+
+```
+{
+  "singleQuote": true,
+  "semi": true,
+  "tabWidth": 2,
+  "trailingComma": "all",
+  "printWidth": 80
+}
+```
---
+++ book-frontend/src/components/Header.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+  Button,
+  Container,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <Navbar bg="light" variant="light">
+      <Container>
+        <Link to="/" className="navbar-brand">
+          홈
+        </Link>
+        <Nav className="me-auto">
+          <Link to="/joinForm" className="nav-link">
+            회원가입
+          </Link>
+          <Link to="/loginForm" className="nav-link">
+            로그인
+          </Link>
+          <Link to="/saveForm" className="nav-link">
+            글쓰기
+          </Link>
+        </Nav>
+        {/* <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-info">Search</Button>
+        </Form> */}
+      </Container>
+      <br />
+    </Navbar>
+  );
+};
+
+export default Header;
