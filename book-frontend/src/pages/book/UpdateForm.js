026e7deb918cb4486b36f214c84114040731f0ab
moonstal1506 <moonstal1506@gmail.com>
2022-03-26 11:34:08 +0900

    수정삭제

--- book-frontend/src/pages/book/UpdateForm.js
+++ book-frontend/src/pages/book/UpdateForm.js
@@ -1,10 +1,83 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 
-const UpdateForm = () => {
+const UpdateForm = (props) => {
+  const id = props.match.params.id;
+
+  const [book, setBook] = useState({
+    title: '',
+    author: '',
+  });
+
+  useEffect(() => {
+    fetch('http://localhost:9000/book/' + id)
+      .then((res) => res.json())
+      .then((res) => {
+        setBook(res);
+      });
+  }, []);
+
+  const changeValue = (e) => {
+    setBook({
+      ...book,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const submitBook = (e) => {
+    e.preventDefault(); //submit action안탐
+    fetch('http://localhost:9000/book/' + id, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      body: JSON.stringify(book),
+    })
+      .then((res) => {
+        console.log(1, res);
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          return null;
+        }
+      })
+      .then((res) => {
+        if (res !== null) {
+          props.history.push('/book/' + id);
+        } else {
+          alert('책 수정에 실패하였습니다.');
+        }
+      });
+  };
+
   return (
-    <div>
-      <h1>책 수정하기</h1>
-    </div>
+    <Form onSubmit={submitBook}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter Title"
+          onChange={changeValue}
+          name="title"
+          value={book.title}
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Author</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter Author"
+          onChange={changeValue}
+          name="author"
+          value={book.author}
+        />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 };
 
